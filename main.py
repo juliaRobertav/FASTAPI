19c8ff4740d4d65f44faf83938e9ec6dad53941f
julinha <114812014+juliaRobertav@users.noreply.github.com>
2023-09-04 11:25:53 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI, HTTPException, status
+from fastapi import FastAPI, HTTPException, status, Response #, Path
+from models import Curso
+
 
 app = FastAPI()  
 
@@ -19,16 +21,58 @@ cursos = {
     }
 }
 
+@app.get('/cursos')
+async def get_cursos():
+    return cursos
+
+#é necessário fazer a tipagem de dados
+
+
 @app.get('/cursos/{curso_id}') 
-async def get_cursos(curso_id : int):
+async def get_curso(curso_id : int ):
     try:
         curso = cursos[curso_id]
         curso.update({"id": curso_id})
         return curso
     except KeyError:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Curso Não Encontrado')
+    
+    
+@app.post('/cursos', status_code=status.HTTP_201_CREATED)
+async def post_curso(curso: Curso):
+    next_id = len(cursos) + 1
+    if next_id not in cursos:
+        curso.id = next_id
+        cursos.append(curso)
+        return curso
+    else:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"Já existe um curso com o ID {curso.id}")
+ 
+
+@app.put('/cursos/{curso_id}')    
+async def put_curso(curso_id: int, curso: Curso):
+    if curso_id in cursos:
+        cursos [curso_id] = curso
+        return curso
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Esse Curso Não Existe.")
+ 
+
+@app.delete('/cursos/{curso_id}')
+async def delete_curso(curso_id: int):
+    if curso_id in cursos:
+        del cursos[curso_id]
+        return Response(status_code=status.HTTP_204_NO_CONTENT)
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Esse Curso Não Existe.")
+
+
+@app.get('/calculadora')
+async def calcular(a, b, c):
+    soma = a + b + c
+    return {"resultado": soma}
+
 
 if __name__ == '__main__':
     import uvicorn
     uvicorn.run("main:app", host='127.0.0.1', port=8000, reload=True)
-
