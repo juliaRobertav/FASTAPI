ba2473e1873f0959fcb4f12fe9e8e7c0a5a7728f
julinha <114812014+juliaRobertav@users.noreply.github.com>
2023-09-14 15:56:20 -0300

    Delete models.py

--- models.py
+++
@@ -1,9 +0,0 @@
-from typing import Optional
-from pydantic import BaseModel
-
-class Livro(BaseModel):
-    id: Optional[int] = None
-    nome: str
-    genero: str
-    autor: str
-    editora: str
